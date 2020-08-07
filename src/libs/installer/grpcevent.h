bb27dd229bde3f59577a150a117c40afb6a7e919
Hjalti Kolbeinsson <hjalti@kolbeinsson.se>
2020-08-07 09:21:37 +0000

    grpcevent readd

--- src/libs/installer/grpcevent.h
+++ src/libs/installer/grpcevent.h
@@ -1,20 +1,20 @@
-// #ifndef GRPCEVENT_H
-// #define GRPCEVENT_H
-// #include "qinstallerglobal.h"
+#ifndef GRPCEVENT_H
+#define GRPCEVENT_H
+#include "qinstallerglobal.h"
 
-// #include <QtCore/QObject>
+#include <QtCore/QObject>
 
-// namespace QInstaller {
-// class INSTALLER_EXPORT GrpcEvent : public QObject, public Operation
-// {
-//     Q_OBJECT
-// public:
-//     explicit GrpcEvent(PackageManagerCore *core);
+namespace QInstaller {
+class INSTALLER_EXPORT GrpcEvent : public QObject, public Operation
+{
+    Q_OBJECT
+public:
+    explicit GrpcEvent(PackageManagerCore *core);
 
-//     void backup();
-//     bool performOperation();
-//     bool undoOperation();
-//     bool testOperation();
-// };
-// }
-// #endif // GRPCEVENT_H
+    void backup();
+    bool performOperation();
+    bool undoOperation();
+    bool testOperation();
+};
+}
+#endif // GRPCEVENT_H
